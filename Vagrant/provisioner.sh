ad98dc238edf069409cd7e8bcd26b2b177dc145c
dalton-lee <li.docx@gmail.com>
2017-12-27 20:26:07 +0800

    import rpm sign and replace shell variables

--- Vagrant/provisioner.sh
+++ Vagrant/provisioner.sh
@@ -5,6 +5,7 @@ timedatectl set-timezone Asia/Shanghai
 systemctl restart network
 
 curl http://ThinkPad-P50/ambari/centos7/2.x/updates/2.6.0.0/ambari.repo -o /etc/yum.repos.d/ambari.repo
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 yum -y install glibc-common ntpdate
 localedef --list-archive | egrep -v -i "zh_CN|en_US" | xargs localedef --delete-from-archive
 mv -f /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
@@ -15,7 +16,7 @@ localectl set-locale LANG=zh_CN.utf8
 echo "25/* * * * * /usr/sbin/ntpdate 120.25.115.19" > /var/spool/cron/vagrant
 
 echo "export JAVA_HOME=/vagrant/jdk1.7.0_80" >> /etc/profile
-echo "export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar" >> /etc/profile
-echo "export PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile
+echo "export CLASSPATH=.:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar" >> /etc/profile
+echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> /etc/profile
 
 source /etc/profile
